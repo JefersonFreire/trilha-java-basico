31cd01fdd38a232c038d51f671dbe81db9bd5ee4
JefersonFreire <jfreire86@hotmail.com>
2022-09-06 19:58:01 -0300

    Utilizando concatenação

--- Operadores/src/Concatenacao.java
+++ Operadores/src/Concatenacao.java
@@ -1,6 +1,14 @@
-public class Operadores {
+public class Concatenacao {
 
     public static void main(String[] args) {
+        int numero = 5;
+
+        numero = -numero *-1;
+        System.out.println(-numero);
+        System.out.println(numero);
+        System.out.println(numero);
+
+
         String nomeCompleto = "LINGUAGEM" + " JAVA";
 
         System.out.println(nomeCompleto);
@@ -19,7 +27,7 @@ public class Operadores {
 
         System.out.println(concatenacao);
 
-        concatenacao = "1"+1+1+1;
+        concatenacao = "1"+(1+1+1);
 
         System.out.println(concatenacao);
 
